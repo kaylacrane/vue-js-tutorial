85d29fdf1ca2446ea0d4f59b3e55aa64c9762d06
Kayla Jean Crane <kayla.crane@gmail.com>
2020-09-30 15:27:08 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }}
+    <span><strong>Followers: </strong>{{ followers }}</span>
+    <!-- @ is more or less equivalent to "v-" -->
+    <button @click="followUser">Follow</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "kaylajcrane",
+        firstName: "Kayla",
+        lastName: "Crane",
+        email: "kayla.crane@gmail.com",
+        isAdmin: true,
+      },
+    };
+  },
+  watch: {
+    // can set up where something is updated (like follower count) when new data is received (perhaps from API)
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    },
+  },
+  computed: {
+    // computed property helps make template look cleaner and easier to read
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+  mounted() {
+    // when we first load the component, followUser will run (can do API calls here!)
+    this.followUser();
+  },
+};
 </script>
 
 <style>
@@ -19,8 +54,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 </style>
